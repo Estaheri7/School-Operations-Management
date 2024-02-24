6f4a50f1f2d0a9211d38f33b55f3a0603fe421f2
erfansalman <estaheri101@gmail.com>
2024-02-24 18:51:46 +0330

    added connection method to MySQL

--- databases/mysql_connector.py
+++ databases/mysql_connector.py
@@ -3,4 +3,26 @@ from mysql.connector import Error
 
 
 class MySQLConnector:
-    pass
+    # Connecting to MySQL or Database if not None
+    def __init__(self, host, user, password, db_name=None):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+        self.db = None
+        self.cursor = None
+        self.connect()
+
+    def connect(self):
+        try:
+            self.db = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                databases=self.db_name
+            )
+            self.cursor = self.db.cursor()
+        except Error as e:
+            print(e)
+
+    
