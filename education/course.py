222cb6655e6285d6efca1285507b5e25a2982960
erfansalman <estaheri101@gmail.com>
2024-02-25 21:52:48 +0330

    added new attribute course_code

--- education/course.py
+++ education/course.py
@@ -1,5 +1,7 @@
 class Course:
-    def __init__(self, name, capacity):
+    def __init__(self, name, course_code, capacity):
         self.name = name
+        self.course_code = course_code
         self.capacity = capacity
+
 
