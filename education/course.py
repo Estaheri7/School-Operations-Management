b8d3e6adc9a061bf6e5e85e903a369a5f6d4faa3
erfansalman <estaheri101@gmail.com>
2024-02-25 21:54:02 +0330

    added add_course method and docstrings

--- education/course.py
+++ education/course.py
@@ -1,7 +1,29 @@
 class Course:
     def __init__(self, name, course_code, capacity):
+        """
+        Initializes the MySQLConnector object with the provided parameters.
+
+        :param name: A name for each course object.
+        :param course_code: A unique value for each course.
+        :param capacity: Maximum number of enrollments.
+        """
         self.name = name
         self.course_code = course_code
         self.capacity = capacity
 
+    def add_course(self, database):
+        """
+        Adds new course to school.
 
+        :param database: A MySQLConnector object which is connected to database.
+        """
+        query = """
+            INSERT INTO courses(name, course_code, capacity)
+            VALUES(%s, %s, %s);
+        """
+        values = (self.name, self.course_code, self.capacity)
+        try:
+            database.execute_query(query=query, params=values)
+            database.commit()
+        except:
+            print("Failed to add course")
