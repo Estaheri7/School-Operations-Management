935c488aa6bbf3272623e710ece81d64b5f635c0
erfansalman <estaheri101@gmail.com>
2024-02-25 22:23:31 +0330

    changed course_id to course_code

--- education/classroom.py
+++ education/classroom.py
@@ -1,18 +1,18 @@
 class Classroom:
-    def __init__(self, class_name, current_enrollment, class_code, course_id, teacher_id):
+    def __init__(self, class_name, current_enrollment, class_code, course_code, teacher_id):
         """
         Initializes the MySQLConnector object with the provided parameters.
 
         :param class_name: A string for setting name of the class.
         :param current_enrollment: Number of current enrollments.
         :param class_code: Unique parameter to set code for each classroom.
-        :param course_id: foreign column from course object.
+        :param course_code: foreign column from course object.
         :param teacher_id: foreign column from teacher object.
         """
         self.class_name = class_name
         self.current_enrollment = current_enrollment
         self.class_code = class_code
-        self.course_id = course_id
+        self.course_code = course_code
         self.teacher_id = teacher_id
 
     def add_classroom(self, database):
@@ -29,7 +29,7 @@ class Classroom:
             self.class_name,
             self.current_enrollment,
             self.class_code,
-            self.course_id,
+            self.course_code,
             self.teacher_id
         )
         try:
