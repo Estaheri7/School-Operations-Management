c192aad8842dc53ead068a04da41948c5aea5a9b
erfansalman <estaheri101@gmail.com>
2024-02-25 20:23:54 +0330

    init commit

---
+++ education/classroom.py
@@ -0,0 +1,7 @@
+class Classroom:
+    def __init__(self, class_name, current_enrollment, class_code, course_id, teacher_id):
+        self.class_name = class_name
+        self.current_enrollment = current_enrollment
+        self.class_code = class_code
+        self.course_id = course_id
+        self.teacher_id = teacher_id
---
+++ education/course.py
@@ -0,0 +1,5 @@
+class Course:
+    def __init__(self, name, capacity):
+        self.name = name
+        self.capacity = capacity
+
