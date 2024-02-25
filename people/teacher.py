3ec0bef683497d4d4d8263717fb84a135a393b78
erfansalman <estaheri101@gmail.com>
2024-02-25 22:02:18 +0330

    added department_id

--- people/teacher.py
+++ people/teacher.py
@@ -4,6 +4,7 @@ from .person import *
 class Teacher(Person):
     teacher_id = 0
 
-    def __init__(self, name, email, password, gender):
+    def __init__(self, name, email, password, gender, department_id):
         super().__init__(name, email, password, gender)
+        self.department_id = department_id
         Teacher.teacher_id += 1
