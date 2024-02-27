d61e4980a3d643dd2f0e9b9e38cd4c616f1e88b8
erfansalman <estaheri101@gmail.com>
2024-02-27 10:36:30 +0330

    implemented function to initialize some data

--- data/initialize_data.py
+++ data/initialize_data.py
@@ -4,5 +4,32 @@ from people import Teacher
 
 
 def initialize_data(database, classrooms, courses, teachers):
-    pass
+    with open(classrooms, "r") as file:
+        classrooms = json.load(file)
+
+    with open(courses, "r") as file:
+        courses = json.load(file)
+
+    with open(teachers, "r") as file:
+        teachers = json.load(file)
+
+    for t in teachers:
+        teacher = Teacher(
+            t["name"],
+            t["email"],
+            t["password"],
+            t["gender"],
+            t["teacher_code"],
+            t["department_id"]
+        )
+        teacher.add_teacher(database)
+
+    for c in courses:
+        course = Course(c["name"], c["course_code"], c["capacity"])
+        course.add_course(database)
+
+    for r in classrooms:
+        classroom = Classroom(r["name"], r["current_enrollment"],
+                              r["class_code"], r["course_code"], r["teacher_code"])
+        classroom.add_classroom(database)
 
