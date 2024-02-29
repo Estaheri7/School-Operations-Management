052616640f082e1991edec76a2409d16b521a9cd
erfansalman <estaheri101@gmail.com>
2024-02-29 16:30:11 +0330

    added docstrings for __init__ method

--- people/admin.py
+++ people/admin.py
@@ -3,6 +3,14 @@ from people.person import *
 
 class Admin(Person):
     def __init__(self, name, email, password, gender):
+        """
+        Initializes the Admin object with the provided parameters.
+
+        :param name: Name of the admin,
+        :param email: Unique email address.
+        :param password: Password for admin.
+        :param gender: Gender of new admin.
+        """
         super().__init__(name, email, password, gender)
 
     def add_admin(self):
