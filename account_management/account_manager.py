36827d84e56a327f0900acac05e1e731340fbe94
erfansalman <estaheri101@gmail.com>
2024-02-29 16:50:19 +0330

    added docstring for __init__

--- account_management/account_manager.py
+++ account_management/account_manager.py
@@ -1,3 +1,10 @@
 class AccountManager:
     def __init__(self, database):
+        """
+        Initializes AccountManager object with given database.
+
+        :param database: A MySQLConnector object which is connected to school database.
+        """
         self.database = database
+
+    
