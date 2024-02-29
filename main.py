31768f4d705849b0c2d6838a5694cf4af8c5f4a6
erfansalman <estaheri101@gmail.com>
2024-02-29 17:54:15 +0330

    added role selector and initialize for data

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from databases import MySQLConnector, tables, create_database
+from data.initialize_data import *
+
+
 def main() -> None:
-    pass
+    database = create_database.db
+    try:
+        initialize_data(
+            "data\\classrooms.json",
+            "data\\courses.json",
+            "data\\teachers.json",
+            "data\\students.json",
+            "data\\admins.json"
+        )
+    except:
+        pass
+
+    login = False
+    while True:
+        role = input("Select your role (Student, Teacher, Admin) or -1 to exit: ").lower()
+
+        if role == "student":
+            pass
+        elif role == "teacher":
+            pass
+        elif role == "admin":
+            pass
+        elif role == "-1":
+            pass
+        else:
+            print("Invalid command!")
 
 
 if __name__ == '__main__':
