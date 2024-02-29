41e99372edc74ec5d180fef9c939079c26e91b75
erfansalman <estaheri101@gmail.com>
2024-02-29 17:04:44 +0330

    added a method to check if user is registered

--- account_management/account_manager.py
+++ account_management/account_manager.py
@@ -7,4 +7,12 @@ class AccountManager:
         """
         self.database = database
 
-    
+    def is_registered(self, person, person_code):
+        person_type = type(person).__name__.lower()
+        search_query = f"""
+        SELECT * FROM {person_type + "s"}
+        WHERE email = %s OR {person_type}_code = %s
+        """
+
+        result = self.database.execute_query(query=search_query, params=(person.email, person_code))
+        return result
