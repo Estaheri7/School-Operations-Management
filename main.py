4ecce63af6f8ad7c472d91735720cd5a2039a1a9
erfansalman <estaheri101@gmail.com>
2024-02-29 18:33:01 +0330

    added account_manager object and register statement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from databases import MySQLConnector, tables, create_database
 from data.initialize_data import *
+from people import Admin
+from account_management import *
 
 
 def main() -> None:
@@ -15,18 +17,30 @@ def main() -> None:
     except:
         pass
 
+    ac = AccountManager(database)
+
     login = False
     while True:
         role = input("Select your role (Student, Teacher, Admin) or -1 to exit: ").lower()
+        if role == "-1":
+            break
+
+        pick = int(input("Enter 1 to register or 2 to login: "))
 
         if role == "student":
             pass
         elif role == "teacher":
             pass
         elif role == "admin":
-            pass
-        elif role == "-1":
-            pass
+            if pick == 1:
+                name, email, password, gender, admin_code = Admin.enter_attrs()
+                admin = Admin(name, email, password, gender, admin_code)
+                if not ac.is_registered(admin, admin_code):
+                    admin.add_admin()
+            elif pick == 2:
+                pass
+            else:
+                print("Invalid command!")
         else:
             print("Invalid command!")
 
