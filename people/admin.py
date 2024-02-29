41e9024cedf0af2be3debc7d15f58f713d87cbc0
erfansalman <estaheri101@gmail.com>
2024-02-29 16:28:18 +0330

    added a method to add admin to school

--- people/admin.py
+++ people/admin.py
@@ -5,4 +5,26 @@ class Admin(Person):
     def __init__(self, name, email, password, gender):
         super().__init__(name, email, password, gender)
 
+    def add_admin(self):
+        """
+        Adds new admin to school.
 
+        :raise Exception: If admin cannot be added.
+        """
+        
+        add_query = """
+        INSERT INTO admins(name, email, password, gender)
+        VALUES (%s, %s, %s, %s)
+        """
+
+        values = (
+            self.name,
+            self.email,
+            self.password,
+            self.gender
+        )
+        try:
+            Admin.DB.execute_query(query=add_query, params=values)
+            Admin.DB.commit()
+        except Exception as e:
+            raise e
