960656ab6482360a86fa20e4f92756fd01bbf530
erfansalman <estaheri101@gmail.com>
2024-02-29 17:41:48 +0330

    added docstrings

--- account_management/account_manager.py
+++ account_management/account_manager.py
@@ -8,6 +8,15 @@ class AccountManager:
         self.database = database
 
     def is_registered(self, person, person_code):
+        """
+        Checks if the person is already registered in the system.
+
+        :param person: An object representing the person (e.g., Student or Teacher).
+        :param person_code: The unique identifier (code) of the person.
+
+        :return: True if the person is registered, False otherwise.
+        """
+
         person_type = type(person).__name__.lower()
 
         search_query = f"""
@@ -19,6 +28,14 @@ class AccountManager:
         return result
 
     def can_login(self, person):
+        """
+        Checks if the person can log in with the provided credentials.
+
+        :param person: An object representing the person (e.g., Student or Teacher).
+
+        :return: True if login is successful, False otherwise.
+        """
+
         person_type = type(person).__name__.lower()
 
         search_query = f"""
