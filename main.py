e111a27b0bac18256fb6368c08c0cf508dc9107c
erfansalman <estaheri101@gmail.com>
2024-03-02 10:16:02 +0330

    added login/register for students

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main() -> None:
     except:
         pass
 
-    ac = AccountManager(database)
+    AM = AccountManager(database)
 
     login = False
     while True:
@@ -28,14 +28,33 @@ def main() -> None:
         pick = int(input("Enter 1 to register or 2 to login: "))
 
         if role == "student":
-            pass
+            if pick == 1:
+                student = Student.get_attrs()
+                if not AM.is_registered(student[0], student[0].student_code):
+                    student[0].add_student()
+                    login = True
+                else:
+                    print("This account is already registered!")
+            elif pick == 2:
+                email = input("Enter your email: ")
+                password = input("Enter your password: ")
+                if AM.can_login(role, email, password):
+                    login = True
+                    print("Logged in successfully")
+                else:
+                    print("Invalid email or password!")
+            else:
+                print("Invalid command!")
+            while login:
+                print("done")
+                break
         elif role == "teacher":
             pass
         elif role == "admin":
             if pick == 1:
                 name, email, password, gender, admin_code = Admin.enter_attrs()
                 admin = Admin(name, email, password, gender, admin_code)
-                if not ac.is_registered(admin, admin_code):
+                if not AM.is_registered(admin, admin_code):
                     admin.add_admin()
                     login = True
                     print("Registered successfully!")
@@ -44,7 +63,7 @@ def main() -> None:
             elif pick == 2:
                 email = input("Enter your email: ")
                 password = input("Enter your password: ")
-                let_login = ac.can_login(role, email, password)
+                let_login = AM.can_login(role, email, password)
                 if let_login:
                     login = True
                     print("Logged in successfully!")
