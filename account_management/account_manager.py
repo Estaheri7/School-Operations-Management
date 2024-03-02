3245f657262e3c857b5e9ff65ede9e3418177055
erfansalman <estaheri101@gmail.com>
2024-03-02 20:51:57 +0330

    is_valid_password method added to account_manager class

--- account_management/account_manager.py
+++ account_management/account_manager.py
@@ -64,3 +64,23 @@ class AccountManager:
             return True
         else:
             return False
+
+    @staticmethod
+    def is_valid_password(password):
+        """
+        Validates the format of a password.
+
+        :param password: The password to be validated.
+        :return: True if the password is valid, False otherwise.
+        """
+
+        if len(password) < 8:
+            return False
+
+        if not any(char.isdigit() for char in password):
+            return False
+
+        if not any(char.isalpha() for char in password):
+            return False
+
+        return True
