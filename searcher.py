0afe2f35f572a08cc3e4e0f9c8aee4071d96dae6
erfansalman <estaheri101@gmail.com>
2024-03-03 23:15:20 +0330

    added advance_search and made some changes to it

--- searcher.py
+++ searcher.py
@@ -3,4 +3,37 @@ from people import Student, Admin, Teacher
 
 
 class Searcher:
-    pass
+    @staticmethod
+    def advanced_search(table, criteria=None):
+        """
+        Performs an advanced search on the specified database table based on multiple criteria.
+
+        If no criteria are provided, returns all records from the specified table.
+
+        :param table: A string representing the name of the database table to search.
+        :param criteria: Optional. A dictionary containing the search criteria.
+                         Example: {'class_name': 'Math', 'teacher_code': 123}
+                         Keys represent column names in the table, and values represent desired values
+                         for those columns in the search results.
+
+        :return: A list of tuples containing the records that match the search criteria,
+                 or all records from the specified table if no criteria are provided.
+        """
+
+        if criteria is None or len(criteria) == 0:
+            search_query = f"SELECT * FROM {table}"
+        else:
+            search_query = f"SELECT * FROM {table} WHERE "
+            conditions = []
+
+            for key, value in criteria.items():
+                conditions.append(f"{key} = '{value}'")
+
+            search_query += " AND ".join(conditions)
+
+        try:
+            result = Classroom.DB.execute_query(query=search_query)
+            return result
+        except:
+            print(f"Search failed")
+            return None
