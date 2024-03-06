86680fbf97eab30ffbaeff8f950795efd550d299
erfansalman <estaheri101@gmail.com>
2024-03-06 15:56:59 +0330

    fixed an issue in visualize_grade_distribution for Null value grades

--- data_report.py
+++ data_report.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class DataReport:
-    def __init__(self, title='', xlabel='', ylabel='', color='blue', figsize=(10, 6)):
+    def __init__(self, title='', xlabel='', ylabel='', color='green', figsize=(5, 5)):
         self.title = title
         self.xlabel = xlabel
         self.ylabel = ylabel
@@ -13,7 +13,7 @@ class DataReport:
     def visualize_grade_distribution(self, class_code):
         enrolls = Searcher.advanced_search("student_classes", {"class_code": class_code})
         student_codes = [str(enroll[1]) for enroll in enrolls]
-        grades = [enroll[3] for enroll in enrolls]
+        grades = [enroll[3] if enroll[3] is not None else 0 for enroll in enrolls]
 
         plt.figure(figsize=self.figsize)
         plt.scatter(student_codes, grades, color=self.color, alpha=0.5)
