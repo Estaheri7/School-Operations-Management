7fef776c79c8335e39b887536b106a324595882b
erfansalman <estaheri101@gmail.com>
2024-03-06 16:32:25 +0330

    added conditions to check if classroom exists or its for the current teacher

--- data_report.py
+++ data_report.py
@@ -1,4 +1,5 @@
 from searcher import Searcher
+from education import Classroom
 import matplotlib.pyplot as plt
 
 
@@ -10,8 +11,20 @@ class DataReport:
         self.color = color
         self.figsize = figsize
 
-    def visualize_grade_distribution(self, class_code):
+    def visualize_grade_distribution(self, class_code, teacher_code):
+        result = Classroom.search_by_code(class_code)
+        if not result:
+            print("Classroom not found!")
+            return
+
+        if teacher_code != result[0][5]:
+            print("You don't own this classroom!")
+            return
+
         enrolls = Searcher.advanced_search("student_classes", {"class_code": class_code})
+        if not enrolls:
+            print("Enrolls not found!")
+            return
         student_codes = [str(enroll[1]) for enroll in enrolls]
         grades = [enroll[3] if enroll[3] is not None else 0 for enroll in enrolls]
 
