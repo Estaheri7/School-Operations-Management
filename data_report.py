c7e2fa586ef401838ebbd231167c7b8786f6e537
erfansalman <estaheri101@gmail.com>
2024-03-06 15:09:53 +0330

    initialized constructor for DataReport

--- data_report.py
+++ data_report.py
@@ -1,3 +1,12 @@
+import matplotlib.pyplot as plt
+
+
 class DataReport:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, title='', xlabel='', ylabel='', color='blue', figsize=(10, 6)):
+        self.x = x
+        self.y = y
+        self.title = title
+        self.xlabel = xlabel
+        self.ylabel = ylabel
+        self.color = color
+        self.figsize = figsize
