f2a08741f86e80a4e4bed2efd64bdcd13df72cae
erfansalman <estaheri101@gmail.com>
2024-03-07 17:45:27 +0330

    added comments

--- data_report.py
+++ data_report.py
@@ -46,15 +46,18 @@ class DataReport:
             ValueError: If the specified classroom or teacher is not found.
         """
 
+        # checking if given class_code exists
         result = Classroom.search_by_code(class_code)
         if not result:
             print("Classroom not found!")
             return
 
+        # checking if teacher who sent class code owns the class
         if teacher_code != result[0][5]:
             print("You don't own this classroom!")
             return
 
+        # retrieve enrollment data for the class
         enrolls = Searcher.advanced_search("student_classes", {"class_code": class_code})
         if not enrolls:
             print("Enrolls not found!")
@@ -62,6 +65,7 @@ class DataReport:
         student_codes = [str(enroll[1]) for enroll in enrolls]
         grades = [enroll[3] if enroll[3] is not None else 0 for enroll in enrolls]
 
+        # create scatter plot for grade distribution
         plt.figure(figsize=self.figsize)
         plt.scatter(student_codes, grades, color=self.color, alpha=0.5)
         plt.title(self.title)
@@ -82,13 +86,18 @@ class DataReport:
         :return: None
         """
 
+        # search for classroom if exists...
         classrooms = Searcher.advanced_search("classrooms")
         if not classrooms:
             print("Classrooms not found!")
             return
+
+        # collecting class_codes
         class_codes = [str(enrollment[3]) for enrollment in classrooms]
+        # collecting enrollment numbers
         current_enrollments = [str(enrollment[2]) for enrollment in classrooms]
 
+        # create bar plot for enrollment distribution
         plt.figure(figsize=self.figsize)
         plt.bar(class_codes, current_enrollments, color=self.color)
         plt.title(self.title)
@@ -105,23 +114,29 @@ class DataReport:
         :return: None
         """
 
+        # searching for classroom if exists...
         classrooms = Searcher.advanced_search("classrooms")
         if not classrooms:
             print("Classrooms not found!")
             return
 
+        # converting classroom data to DataFrame
         df = pd.DataFrame(classrooms,
                           columns=['class_id', 'class_name', 'current_enrollment', 'class_code', 'course_code',
                                    'teacher_code'])
-        
+
+        # group data by teacher and aggregate course count and total students
         teacher_workload = df.groupby('teacher_code').agg(
             {'course_code': 'nunique', 'current_enrollment': 'sum'}).reset_index()
         teacher_workload.columns = ['teacher_code', 'num_courses', 'total_students']
 
+        # retrieve teacher names from Searcher
         teacher_names = {teacher[5]: teacher[1] for teacher in Searcher.advanced_search("teachers")}
 
+        # map teacher names to teacher workload DataFrame
         teacher_workload['teacher_name'] = teacher_workload['teacher_code'].map(teacher_names)
 
+        # create bar plot for teacher workload
         plt.figure(figsize=self.figsize)
         plt.bar(teacher_workload['teacher_name'], teacher_workload['num_courses'], color='blue',
                 label='Number of Courses')
