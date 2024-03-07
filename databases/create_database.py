b24abb99c095dfc4f384e0d5fe6f03b4d44a4d32
erfansalman <estaheri101@gmail.com>
2024-03-07 16:29:35 +0330

    added comments

--- databases/create_database.py
+++ databases/create_database.py
@@ -2,6 +2,9 @@ from databases.mysql_connector import MySQLConnector
 from databases.table_queries import tables
 import json
 
+# By importing this file to main and using MySQLConnector class we create fresh database and tables.
+# We can use db variable later in main file.
+
 with open("databases/db_info.json", "r") as file:
     db_info = json.load(file)
 db = MySQLConnector(db_info["host"], db_info["user"], db_info["password"])
