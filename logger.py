e729051365f00a6058c8a795f26555ac01b58572
erfansalman <estaheri101@gmail.com>
2024-03-08 15:25:38 +0330

    added configure_logging method and log method

--- logger.py
+++ logger.py
@@ -5,4 +5,15 @@ import os
 class Logger:
     def __init__(self, log_dir="logs"):
         self.log_dir = log_dir
+        self.configure_logging()
 
+    def configure_logging(self):
+        log_path = os.path.join(self.log_dir, "error.log")
+        logging.basicConfig(
+            filename=log_path,
+            level=logging.DEBUG,
+            format="%(asctime)s - %(levelname)s - %(message)s"
+        )
+
+    def log(self, message):
+        logging.info(message)
